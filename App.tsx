c8b2d3a73a8afd44e9479d7d153df6ae597191c3
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-04-20 08:56:46 -0300

    chore: add expo app loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native'
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 import { Welcome } from './src/pages/welcome';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
   return (
     <Welcome />
   )
